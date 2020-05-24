30903130a0c8446e8923213164615091df7c623e
cbwang2016 <cbwang2016@outlook.com>
2020-05-24 12:34:33 +0800

    enhancement

--- README.md
+++ README.md
@@ -1,13 +1,37 @@
 # WebWechatReverseProxy
 带有撤回提醒小助手的Web Wechat反向代理。
 
-![](./imgs/show.png)
+![https://i.loli.net/2020/05/24/lGjE6PWs9HutgIm.png](https://i.loli.net/2020/05/24/lGjE6PWs9HutgIm.png)
+## Features
+ - Notify you the messages recalled by your friend.
+ - Let you use web wechat client normally while this bot is online.
+ - If you have a server(e.g. raspberry pi), you can stay online persistently. 
+ - You can also use nginx to make it accessible from outside localhost
+ 
+## Installations
+```
+pip install -r requirements.txt
+```
 
-# Installation
-TODO
+## Usage
+```
+>python main.py -h
+usage: main.py [-h] [-a ADDRESS] [-p PORT] [-o HOST] [-s] [-d] [-q {0,1,2}]
 
-# Usage
-TODO
+wx.qq.com reverse proxy.
 
-# Known Issues
-TODO
+optional arguments:
+  -h, --help            show this help message and exit
+  -a ADDRESS, --address ADDRESS
+                        the address of this web server
+  -p PORT, --port PORT  listening port
+  -o HOST, --host HOST  listening host
+  -s, --https           whether this web server uses https
+  -d, --debug           use flask debug
+  -q {0,1,2}            show qrcode in command line; integers can be used to
+                        fit strange char length
+
+```
+
+## Licence
+[GPLv3](./LICENSE)
--- main.py
+++ main.py
@@ -1,239 +1,171 @@
 # coding:utf-8
+import logging
 import os
 import re
-import shutil
 import time
 from collections import OrderedDict
 
+import coloredlogs
 import itchat
-from apscheduler.schedulers.blocking import BlockingScheduler
 from flask import Flask, Response, request
 from itchat.content import *
 
-rec_tmp_dir = os.path.join(os.getcwd(), 'tmp/')
-msg_information = {}
-face_bug = None  # 针对表情包的内容
-sched = BlockingScheduler()
+coloredlogs.install()
+rev_tmp_dir = os.path.join(os.getcwd(), 'tmp/')
+msg_dict = {}
 
 
-def clear_cache():
-    # 当前时间
-    print('Clearing cache...')
-    cur_time = time.time()
-    # 遍历字典，如果有创建时间超过2分钟(120s)的记录，删除，非文本的话，连文件也删除
-    for key in list(msg_information.keys()):
-        if int(cur_time) - int(msg_information.get(key).get('msg_time')) > 120:
-            if msg_information.get(key).get('msg_type') == "Attachment" \
-                    or msg_information.get(key).get('msg_type') == "Video" \
-                    or msg_information.get(key).get('msg_type') == 'Picture' \
-                    or msg_information.get(key).get('msg_type') == 'Recording':
-                file_path = os.path.join(
-                    rec_tmp_dir, msg_information.get(key).get('msg_content'))
-                # print('Deleting ' + file_path)
-                if os.path.exists(file_path):
-                    os.remove(file_path)
-            msg_information.pop(key)
+@itchat.msg_register([TEXT, PICTURE, MAP, CARD, SHARING, RECORDING, ATTACHMENT, VIDEO, FRIENDS], isFriendChat=True,
+                     isGroupChat=True, isMpChat=True)
+# 定义消息ID和消息时间，并且加以判断是群消息或者是好友消息
+def handler_receive_msg(msg):
+    # 每条消息id
+    msg_id = msg['MsgId']
+    # 接受消息的时间
+    msg_time_rec = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    # 消息发送的时间
+    msg_time = msg['CreateTime']
+    # 储存信息的内容
+    msg_content = None
+    # 储存分享的链接，比如分享的文章和音乐
+    msg_share_url = None
 
+    group_name = group_members = ''
 
-def start_schedule():
-    sched.add_job(clear_cache, 'interval', minutes=2)
-    sched.start()
+    # 判断是群消息或者是好友消息
+    if 'ActualNickName' in msg:
+        from_user = msg['ActualUserName']  # 群消息的发送者,用户的唯一标识
+        msg_from = msg['ActualNickName']  # 发消息人的群内昵称
+        # 获取所有好友
+        friends = itchat.get_friends(update=True)
 
-
-def after_logout():
-    sched.shutdown()
-    shutil.rmtree(rec_tmp_dir)
-
-
-@itchat.msg_register([TEXT, PICTURE, FRIENDS, CARD, MAP, SHARING, RECORDING, ATTACHMENT, VIDEO], isFriendChat=True,
-                     isMpChat=True)
-def handle_receive_msg(msg):
-    global face_bug
-    msg_time_rec = time.strftime(
-        "%Y-%m-%d %H:%M:%S", time.localtime())  # 接受消息的时间
-    msg_from = itchat.search_friends(userName=msg['FromUserName'])[
-        'NickName']  # 在好友列表中查询发送信息的好友昵称
-    msg_time = msg['CreateTime']  # 信息发送的时间
-    msg_id = msg['MsgId']  # 每条信息的id
-    msg_content = None  # 储存信息的内容
-    msg_share_url = None  # 储存分享的链接，比如分享的文章和音乐
-    print(msg['Type'])
-    print(msg['MsgId'])
-    if msg['Type'] == 'Text' or msg['Type'] == 'Friends':  # 如果发送的消息是文本或者好友推荐
-        msg_content = msg['Text']
-        print(msg_content)
-
-    # 如果发送的消息是附件、视屏、图片、语音
-    elif msg['Type'] == "Attachment" or msg['Type'] == "Video" \
-            or msg['Type'] == 'Picture' \
-            or msg['Type'] == 'Recording':
-        msg_content = msg['FileName']  # 内容就是他们的文件名
-        msg['Text'](rec_tmp_dir + msg['FileName'])  # 下载文件
-        # print( msg_content
-    elif msg['Type'] == 'Card':  # 如果消息是推荐的名片
-        msg_content = msg['RecommendInfo']['NickName'] + '的名片'  # 内容就是推荐人的昵称和性别
-        if msg['RecommendInfo']['Sex'] == 1:
-            msg_content += '性别为男'
+        for friend in friends:
+            if from_user == friend['UserName']:  # 判断群里撤回消息的是否为自己好友
+                if friend['RemarkName']:  # 优先使用好友的备注名称，没有则使用昵称
+                    msg_from = friend['RemarkName']
+                else:
+                    msg_from = friend['NickName']
+                break
+        if msg['User']:
+            group_name = msg['User']['NickName']
+            group_members = msg['User']['MemberCount']
         else:
-            msg_content += '性别为女'
+            # 获取所有的群
+            groups = itchat.get_chatrooms(update=True)
+            for group in groups:
+                if msg['FromUserName'] == group['UserName']:  # 根据群消息的FromUserName匹配是哪个群
+                    group_name = group['NickName']
+                    group_members = group['MemberCount']
+                    break
 
-        print(msg_content)
-    elif msg['Type'] == 'Map':  # 如果消息为分享的位置信息
-        x, y, location = re.search(
-            "<location x=\"(.*?)\" y=\"(.*?)\".*label=\"(.*?)\".*", msg['OriContent']).group(1, 2, 3)
+        if group_members:
+            group_name = (group_name + "(" + str(group_members) + ")")
+        else:
+            group_name = 'failed to get group name!'
+            logging.warning('failed to get group name!!')
+            logging.warning(str(msg))
+    # 否则是个人好友消息
+    else:
+        if itchat.search_friends(userName=msg['FromUserName'])['RemarkName']:  # 优先使用备注名称
+            msg_from = itchat.search_friends(userName=msg['FromUserName'])['RemarkName']
+        else:
+            msg_from = itchat.search_friends(userName=msg['FromUserName'])['NickName']  # 在好友列表中查询发送信息的好友昵称
+
+    if msg['Type'] in ('Text', 'Friends'):
+        msg_content = msg['Text']
+
+        logging.info('[Text/Friends]: %s' % msg_content)
+    elif msg['Type'] in ('Recording', 'Attachment', 'Video', 'Picture'):
+        msg_content = r"" + msg['FileName']
+        msg['Text'](rev_tmp_dir + msg_content)  # 保存文件
+
+        logging.info('[Attachment/Video/Picture/Recording]: %s' % msg_content)
+    elif msg['Type'] == 'Card':
+        msg_content = msg['RecommendInfo']['NickName'] + r" 的名片"  # 内容就是推荐人的昵称和性别
+        if msg['RecommendInfo']['Sex'] == 1:
+            msg_content += '，性别为男'
+        else:
+            msg_content += '，性别为女'
+
+        logging.info('[Card]: %s' % msg_content)
+    elif msg['Type'] == 'Map':
+        x, y, location = re.search("<location x=\"(.*?)\" y=\"(.*?)\".*label=\"(.*?)\".*", msg['OriContent']).group(1,
+                                                                                                                    2,
+                                                                                                                    3)
         if location is None:
-            msg_content = r"纬度->" + x.__str__() + " 经度->" + y.__str__()  # 内容为详细的地址
+            msg_content = r"纬度->" + x.__str__() + " 经度->" + y.__str__()
         else:
             msg_content = r"" + location
-    elif msg['Type'] == 'Sharing':  # 如果消息为分享的音乐或者文章，详细的内容为文章的标题或者是分享的名字
+
+        logging.info('[Map]: %s' % msg_content)
+    elif msg['Type'] == 'Sharing':
         msg_content = msg['Text']
         msg_share_url = msg['Url']  # 记录分享的url
-        print(msg_share_url)
-    face_bug = msg_content
 
-    # 将信息存储在字典中，每一个msg_id对应一条信息
-    msg_information.update(
+        logging.info('[Sharing]: %s' % msg_content)
+
+    # 更新字典
+    msg_dict.update(
         {
             msg_id: {
-                "msg_from": msg_from, "msg_time": msg_time, "msg_time_rec": msg_time_rec,
+                "msg_from": msg_from,
+                "msg_time": msg_time,
+                "msg_time_rec": msg_time_rec,
                 "msg_type": msg["Type"],
-                "msg_content": msg_content, "msg_share_url": msg_share_url
+                "msg_content": msg_content,
+                "msg_share_url": msg_share_url,
+                "group_name": group_name
             }
         }
     )
+
+    # 自动删除130秒之前的消息，避免数据量太大后引起内存不足
+    del_info = []
+    for k in msg_dict:
+        m_time = msg_dict[k]['msg_time']  # 取得消息时间
+        if int(time.time()) - m_time > 130:
+            del_info.append(k)
+    if del_info:
+        for i in del_info:
+            msg_dict.pop(i)
 
 
 # 这个是用于监听是否有friend消息撤回
 @itchat.msg_register(NOTE, isFriendChat=True, isGroupChat=True, isMpChat=True)
-def information(msg):
-    # 这里如果这里的msg['Content']中包含消息撤回和id，就执行下面的语句
-    if '撤回了一条消息' in msg['Content']:
-        # 在返回的content查找撤回的消息的id
-        old_msg_id = re.search(
-            "\<msgid\>(.*?)\<\/msgid\>", msg['Content']).group(1)
-        old_msg = msg_information.get(old_msg_id)  # 得到消息
-        print(old_msg)
-        if len(old_msg_id) < 11:  # 如果发送的是表情包
-            itchat.send_file(face_bug, toUserName='filehelper')
-        else:  # 发送撤回的提示给文件助手
-            msg_body = "【" \
-                       + old_msg.get('msg_from') + " 撤回了 】\n" \
-                       + old_msg.get("msg_type") + " 消息：" + "\n" \
-                       + old_msg.get('msg_time_rec') + "\n" \
-                       + r"" + old_msg.get('msg_content')
-            # 如果是分享的文件被撤回了，那么就将分享的url加在msg_body中发送给文件助手
-            if old_msg['msg_type'] == "Sharing":
-                msg_body += "\n就是这个链接➣ " + old_msg.get('msg_share_url')
+# 收到note通知类消息，判断是不是撤回并进行相应操作
+def send_msg_helper(msg):
+    if re.search(r"\<\!\[CDATA\[.*撤回了一条消息\]\]\>", msg['Content']) is not None:
+        # 获取消息id
+        old_msg_id = re.search("\<msgid\>(.*?)\<\/msgid\>", msg['Content']).group(1)
+        # 得到消息
+        old_msg = msg_dict.get(old_msg_id)
+        logging.info('[Recall]: %s' % old_msg)
 
-            # 将撤回消息发送到文件助手
-            itchat.send_msg(msg_body, toUserName='filehelper')
-            # 有文件的话也要将文件发送回去
-            if old_msg["msg_type"] == "Picture" \
-                    or old_msg["msg_type"] == "Recording" \
-                    or old_msg["msg_type"] == "Video" \
-                    or old_msg["msg_type"] == "Attachment":
-                file = '@fil@%s' % (rec_tmp_dir + old_msg['msg_content'])
-                itchat.send(msg=file, toUserName='filehelper')
-                os.remove(rec_tmp_dir + old_msg['msg_content'])
-            # 删除字典旧消息
-            msg_information.pop(old_msg_id)
+        msg_body = "告诉你一个秘密~" '\n' \
+                   + "【" + old_msg.get('msg_from') + "】" + " 撤回了一条消息！ " + '\n' \
+                   + '➣消息类型：' + old_msg.get("msg_type") + '\n' \
+                   + '➣时间：' + old_msg.get('msg_time_rec') + '\n' \
+                   + (('➣群名：' + old_msg.get('group_name') + '\n') if old_msg.get('group_name') else '') \
+                   + '快看看ta撤回了什么?' + '\n' \
+                   + '⇣' + '\n' \
+                   + r"【%s】" % old_msg.get('msg_content')
 
+        # 如果分享的文件被撤回了，那么就将分享的url加在msg_body中发送给文件助手
+        if old_msg['msg_type'] == "Sharing":
+            msg_body += "\n就是这个链接➣ " + old_msg.get('msg_share_url')
 
-@itchat.msg_register([TEXT, PICTURE, FRIENDS, CARD, MAP, SHARING, RECORDING, ATTACHMENT, VIDEO], isGroupChat=True)
-def handle_receive_msg2(msg):
-    global face_bug
-    msg_time_rec = time.strftime(
-        "%Y-%m-%d %H:%M:%S", time.localtime())  # 接受消息的时间
-    # groupid = msg['FromUserName']
-    # chatroom = itchat.search_chatrooms(userName=groupid)
-    msg_Actual_from = msg['ActualNickName']
-    # msg_Actual_from = msg['User']
-    # msg_from = msg_Actual_from['Self']['NickName']
-    msg_from = msg_Actual_from
-    msg_time = msg['CreateTime']  # 信息发送的时间
-    msg_id = msg['MsgId']  # 每条信息的id
-    msg_content = None  # 储存信息的内容
-    msg_share_url = None  # 储存分享的链接，比如分享的文章和音乐
-    print(msg['Type'])
-    print(msg['MsgId'])
-    if msg['Type'] == 'Text' or msg['Type'] == 'Friends':  # 如果发送的消息是文本或者好友推荐
-        msg_content = msg['Text']
-        print(msg_content)
+        # 将撤回消息发送到文0 件助手
+        itchat.send(msg_body, toUserName='filehelper')
+        logging.info(msg_body)
 
-    # 如果发送的消息是附件、视屏、图片、语音
-    elif msg['Type'] == "Attachment" or msg['Type'] == "Video" \
-            or msg['Type'] == 'Picture' \
-            or msg['Type'] == 'Recording':
-        msg_content = msg['FileName']  # 内容就是他们的文件名
-        msg['Text'](rec_tmp_dir + msg['FileName'])  # 下载文件
-        # print( msg_content
-    elif msg['Type'] == 'Card':  # 如果消息是推荐的名片
-        msg_content = msg['RecommendInfo']['NickName'] + '的名片'  # 内容就是推荐人的昵称和性别
-        if msg['RecommendInfo']['Sex'] == 1:
-            msg_content += '性别为男'
-        else:
-            msg_content += '性别为女'
+        # 有文件的话也要将文件发送回去
+        if old_msg["msg_type"] in ("Picture", "Recording", "Video", "Attachment"):
+            file = '@fil@%s' % (rev_tmp_dir + old_msg['msg_content'])
+            itchat.send(msg=file, toUserName='filehelper')
+            os.remove(rev_tmp_dir + old_msg['msg_content'])
 
-        print(msg_content)
-    elif msg['Type'] == 'Map':  # 如果消息为分享的位置信息
-        x, y, location = re.search(
-            "<location x=\"(.*?)\" y=\"(.*?)\".*label=\"(.*?)\".*", msg['OriContent']).group(1, 2, 3)
-        if location is None:
-            msg_content = r"纬度->" + x.__str__() + " 经度->" + y.__str__()  # 内容为详细的地址
-        else:
-            msg_content = r"" + location
-    elif msg['Type'] == 'Sharing':  # 如果消息为分享的音乐或者文章，详细的内容为文章的标题或者是分享的名字
-        msg_content = msg['Text']
-        msg_share_url = msg['Url']  # 记录分享的url
-        print(msg_share_url)
-    face_bug = msg_content
-
-    # 将信息存储在字典中，每一个msg_id对应一条信息
-    msg_information.update(
-        {
-            msg_id: {
-                "msg_from": msg_from, "msg_time": msg_time, "msg_time_rec": msg_time_rec,
-                "msg_type": msg["Type"],
-                "msg_content": msg_content, "msg_share_url": msg_share_url
-            }
-        }
-    )
-
-
-# 这个是用于监听是否有Group消息撤回
-@itchat.msg_register(NOTE, isGroupChat=True, isMpChat=True)
-def information2(msg):
-    # 这里如果这里的msg['Content']中包含消息撤回和id，就执行下面的语句
-    if '撤回了一条消息' in msg['Content']:
-        # 在返回的content查找撤回的消息的id
-        old_msg_id = re.search(
-            "\<msgid\>(.*?)\<\/msgid\>", msg['Content']).group(1)
-        old_msg = msg_information.get(old_msg_id)  # 得到消息
-        print(old_msg)
-        if len(old_msg_id) < 11:  # 如果发送的是表情包
-            itchat.send_file(face_bug, toUserName='filehelper')
-        else:  # 发送撤回的提示给文件助手
-            msg_body = "【" \
-                       + old_msg.get('msg_from') + " 群消息撤回提醒】\n" \
-                       + " 撤回了 " + old_msg.get("msg_type") + " 消息：" + "\n" \
-                       + old_msg.get('msg_time_rec') + "\n" \
-                       + r"" + old_msg.get('msg_content')
-            # 如果是分享的文件被撤回了，那么就将分享的url加在msg_body中发送给文件助手
-            if old_msg['msg_type'] == "Sharing":
-                msg_body += "\n就是这个链接➣ " + old_msg.get('msg_share_url')
-
-            # 将撤回消息发送到文件助手
-            itchat.send_msg(msg_body, toUserName='filehelper')
-            # 有文件的话也要将文件发送回去
-            if old_msg["msg_type"] == "Picture" \
-                    or old_msg["msg_type"] == "Recording" \
-                    or old_msg["msg_type"] == "Video" \
-                    or old_msg["msg_type"] == "Attachment":
-                file = '@fil@%s' % (rec_tmp_dir + old_msg['msg_content'])
-                itchat.send(msg=file, toUserName='filehelper')
-                os.remove(rec_tmp_dir + old_msg['msg_content'])
-            # 删除字典旧消息
-            msg_information.pop(old_msg_id)
+        # 删除字典旧信息
+        msg_dict.pop(old_msg_id)
 
 
 app = Flask(__name__)
@@ -257,9 +189,9 @@ def show_subpath(subpath):
     # show the subpath after /path/
     if 'webwxstatreport' in subpath:  # block stat report
         return ''
-    # if 'webwxlogout' in subpath:
-    #     print('\n\n\n\n\n\n\nERROR??????????!!!!!!!!!!!!!!!!!!!!!\n\n\n\n\n\n\n')
-    #     return ''
+    if 'webwxlogout' in subpath:  # block logout
+        logging.warn('Warning: logout detected!')
+        return ''
 
     data = request.data
 
@@ -273,7 +205,6 @@ def show_subpath(subpath):
     else:
         base_url = WX_URL
     url = base_url + subpath
-    # print('\nurl =', url, '\n')
 
     if request.content_type:
         headers = {'Content-Type': request.content_type,
@@ -305,9 +236,10 @@ def show_subpath(subpath):
     try:
         rtn = r.content.decode('utf-8').replace('webpush.weixin.qq.com', LOCAL_URL).replace(
             'res.wx.qq.com', LOCAL_URL).replace('file.wx.qq.com', LOCAL_URL).replace('js.aq.qq.com',
-                                                                                     '127.0.0.1').replace('https://',
-                                                                                                          'http://')
-        # rtn = rtn.replace('loginout:', 'hahaha:')  # ban logout function
+                                                                                     '127.0.0.1')
+        if not args.https:
+            rtn = rtn.replace('https://', 'http://')
+        rtn = rtn.replace('loginout:', 'hahaha:')  # ban logout function
     except:
         rtn = r.content
 
@@ -319,18 +251,32 @@ def show_subpath(subpath):
 
 
 if __name__ == '__main__':
+    import argparse
 
-    print('main started!')
-    if not os.path.exists(rec_tmp_dir):
-        os.mkdir(rec_tmp_dir)
-    itchat.auto_login(hotReload=True, exitCallback=after_logout, enableCmdQR=False)
+    parser = argparse.ArgumentParser(description="wx.qq.com reverse proxy.")
+    parser.add_argument("-a", "--address", type=str, help="the address of this web server", default="127.0.0.1:5000")
+    parser.add_argument("-p", "--port", type=int, help="listening port", default=5000)
+    parser.add_argument("-o", "--host", type=str, help="listening host", default="127.0.0.1")
+    parser.add_argument("-s", '--https', action="store_true", help="whether this web server uses https")
+    parser.add_argument("-d", '--debug', action="store_true", help="use flask debug")
+    parser.add_argument("-q", type=int, choices=[0, 1, 2],
+                        help="show qrcode in command line; integers can be used to fit strange char length",
+                        default=2)
+    args = parser.parse_args()
+    LOCAL_URL = args.address
+
+    logging.info('main started!')
+    if not os.path.exists(rev_tmp_dir):
+        os.mkdir(rev_tmp_dir)
+
+    itchat.auto_login(hotReload=True, enableCmdQR=args.q)
     itchat.run(blockThread=False)
-    print('itchat started!')
+    logging.info('itchat started!')
 
-    import threading
+    # import threading
+    #
+    # ScheduleThread = threading.Thread(target=start_schedule)
+    # ScheduleThread.setDaemon(True)
+    # ScheduleThread.start()
 
-    ScheduleThread = threading.Thread(target=start_schedule)
-    ScheduleThread.setDaemon(True)
-    ScheduleThread.start()
-
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=args.debug, host=args.host, port=args.port)
